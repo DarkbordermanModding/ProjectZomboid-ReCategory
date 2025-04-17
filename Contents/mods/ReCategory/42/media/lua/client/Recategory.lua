c9bab37ccdaa5cc79bb9550434fd7dbd6bf608dd
Darkborderman <reastw1234@gmail.com>
2025-04-18 00:33:42 +0800

    Update

--- Contents/mods/ReCategory/42/media/lua/client/Recategory.lua
+++ Contents/mods/ReCategory/42/media/lua/client/Recategory.lua
@@ -1,14 +1,17 @@
-local sm = getScriptManager()
+ReCategory = {}
 
--- debug override to check missing recategory item
--- local allItems = getScriptManager():getAllItems()
--- for i=0, allItems:size()-1 do
---   local item = allItems:get(i)
---   item:DoParam("DisplayCategory=" .. "Recat_Debug")
--- end
-
-for category, items in pairs(DBMRecategoryDefinition) do
-  for _, item in ipairs(items) do
-    sm:getItem(item):DoParam("DisplayCategory=" .. category)
+function ReCategory.OnGameBoot()
+  local sm = getScriptManager()
+  -- local allItems = getScriptManager():getAllItems()
+  -- for i=0, allItems:size()-1 do
+  --   local item = allItems:get(i)
+  --   item:DoParam("DisplayCategory=" .. "Recat_Debug")
+  -- end
+  for category, items in pairs(DBMRecategoryDefinition) do
+    for _, item in ipairs(items) do
+      sm:getItem(item):DoParam("DisplayCategory=" .. category)
+    end
   end
 end
+
+Events.OnGameBoot.Add(ReCategory.OnGameBoot)
