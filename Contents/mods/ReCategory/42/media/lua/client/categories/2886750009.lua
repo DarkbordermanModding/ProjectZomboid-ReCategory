2567e3fdb2cf9701a3b9a1aa2b6c314944ac881c
Darkborderman <reastw1234@gmail.com>
2025-05-03 00:01:35 +0800

    Update

--- Contents/mods/ReCategory/42/media/lua/client/categories/2886750009.lua
+++ Contents/mods/ReCategory/42/media/lua/client/categories/2886750009.lua
@@ -1,14 +1,16 @@
 -- VanillaAdjustments
 -- https://steamcommunity.com/sharedfiles/filedetails/?id=2886750009
 require("recategory")
-Definition2886750009 = {
-  modId = "\\darkborderman.VanillaAdjustments",
-  categories = {
-    Recat_LiquidContainer_Medium = {
-      "Base.BeerBottle",
-      "Base.BeerImported",
+
+table.insert(
+  ReCategoryDefinitions,
+  {
+    modId = "\\darkborderman.VanillaAdjustments",
+    categories = {
+      Recat_LiquidContainer_Medium = {
+        "Base.BeerBottle",
+        "Base.BeerImported",
+      }
     }
   }
-}
-
-table.insert(ReCategoryDefinitions, Definition2886750009)
+)
